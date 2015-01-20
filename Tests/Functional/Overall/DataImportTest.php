544fa9620d87c4e29f5a5e54f442aa0764912dfd
Gediminas Šaltenis <gediminas.saltenis@ongr.io>
2015-01-20 16:11:24 +0200

    Improve tests quality

--- Tests/Functional/Overall/DataImportTest.php
+++ Tests/Functional/Overall/DataImportTest.php
@@ -35,11 +35,6 @@ class DataImportTest extends TestBase
 
         $this->application = new Application($this->getClient()->getKernel());
 
-//        $this->getServiceContainer()
-//            ->get('es.manager')
-//            ->getConnection()
-//            ->dropAndCreateIndex();
-
         // Update some data.
         $this->importData('DataImportTest/updateProducts.sql');
     }
