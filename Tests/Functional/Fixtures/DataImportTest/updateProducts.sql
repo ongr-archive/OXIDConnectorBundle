e51d1e1649ea0b5b6ac63acbaf2c36c307ee606e
Gytis Karčiauskas <gytis@karciauskas.lt>
2015-01-26 13:03:18 +0200

    Rename 'desc' to 'description'.

--- Tests/Functional/Fixtures/DataImportTest/updateProducts.sql
+++ Tests/Functional/Fixtures/DataImportTest/updateProducts.sql
@@ -1,6 +1,6 @@
 UPDATE `oxarticles` SET
   `OXTITLE` = 'The same title for all!',
-  `OXSHORTDESC` = 'The same desc for all!';
+  `OXSHORTDESC` = 'The same description for all!';
 
 INSERT INTO `oxarticles` (OXID) VALUES ('id0');
 INSERT INTO `oxarticles` (OXID) VALUES ('id1');
